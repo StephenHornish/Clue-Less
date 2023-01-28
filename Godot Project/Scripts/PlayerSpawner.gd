3adc27c02f6a6b7070180832cf49aa657b2daaa0
Stephen Hornish <hornishs18@gmail.com>
2023-01-28 12:38:40 -0800

    Pawn Color matches Character selection

--- Godot Project/Scripts/PlayerSpawner.gd
+++ Godot Project/Scripts/PlayerSpawner.gd
@@ -2,8 +2,68 @@ extends Node
 
 # Need to set the starting locations based on which players are actually 
 # playing 
-func _ready():
-	var scene = load("res://Characters/Pawn.tscn")
-	var player = scene.instance()
-	add_child(player)
 
+var scene = load("res://Characters/Pawn.tscn")
+func _ready():
+	pass
+
+
+
+func _on_PeacockButton_button_up():
+	var player = scene.instance()
+	var color = Color( 0, 0.501961, 0.501961, 1 )
+	player.set_color(color)
+	add_child(player)
+	var menu = get_node("Scene Control/Character Selection")
+	menu.queue_free()
+	pass # Replace with function body.
+
+
+func _on_ScarlettButton_button_up():
+	var player = scene.instance()
+	var color = Color( 0.9, 0, 0, 1 )  
+	player.set_color(color)
+	add_child(player)
+	var menu = get_node("Scene Control/Character Selection")
+	menu.queue_free()
+	pass # R	pass # Replace with function body.
+
+
+func _on_WhiteButton_button_up():
+	var player = scene.instance()
+	var color = Color( 0.980392, 0.921569, 0.843137, 1 ) 
+	player.set_color(color)
+	add_child(player)
+	var menu = get_node("Scene Control/Character Selection")
+	menu.queue_free()
+	pass # Replace with function body.
+
+
+func _on_GreenButton_button_up():
+	var player = scene.instance()
+	var color =  Color( 0.133333, 0.545098, 0.133333, 1 )
+	player.set_color(color)
+	add_child(player)
+	var menu = get_node("Scene Control/Character Selection")
+	menu.queue_free()
+	pass # Rpass # Replace with function body.
+
+
+func _on_MustardButton_button_up():
+	var player = scene.instance()
+	var color = Color( 0.8, 0.9, 0, 1 ) 
+	player.set_color(color)
+	add_child(player)
+	var menu = get_node("Scene Control/Character Selection")
+	menu.queue_free()
+	pass # Rpass # Replace with function body.
+
+
+func _on_PlumbButton_button_up():
+	var player = scene.instance()
+	var color = Color( 0.576471, 0.439216, 0.858824, 1 )
+	player.set_color(color)
+	add_child(player)
+	var menu = get_node("Scene Control/Character Selection")
+	menu.queue_free()
+	pass # Rpass # Replace with function body.
