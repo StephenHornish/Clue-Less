0ad7b5235a710d74a97990ac8387386f4a5460a8
Stephen Hornish <hornishs18@gmail.com>
2023-02-06 08:43:17 -0800

    Finished creating new RoomDatabase

--- Godot Project/Scripts/RoomData.gd
+++ Godot Project/Scripts/RoomData.gd
@@ -19,7 +19,18 @@ func _init():
 	board.append(tile.new("Kitchen", ["BURHall", "BRHall","Study"], ["Up", "Left","Secret Passage"],false, Vector3( -26 ,0, -22.5)))
 	board.append(tile.new("BillardsRoom", ["MLHall", "TDMHall","MRHall","BUMHall"], ["Left", "Up", "Right","Down"],false, Vector3( -2.3 ,0, -0.5)))
 	
-	
+	board.append(tile.new("TLHall", ["Study","Hall"], ["Left","Right"],true, Vector3( 9.5 ,0, 22.3)))
+	board.append(tile.new("TRHall", ["Hall","Lounge"], ["Left","Right"],true, Vector3( -13 ,0, 22.3)))
+	board.append(tile.new("MRHall", ["BillardsRoom","DinningRoom"], ["Left","Right"],true, Vector3( -2.3 ,0, -0.5)))
+	board.append(tile.new("MLHall", ["Library","BillardsRoom"], ["Left","Right"],true,  Vector3( 9.6,0, -0.01)))
+	board.append(tile.new("BLHall", ["Conservatory","BallRoom"], ["Left","Right"],true,  Vector3( 9.3 ,0, -22.3)))
+	board.append(tile.new("BRHall", ["BallRoom","Kitchen"], ["Left","Right"],true,  Vector3( -14 ,0, -22.3)))
+	board.append(tile.new("BURHall", ["Kitchen","DinningRoom"], ["Up","Down"],true,  Vector3( -25 ,0, -11.2)))
+	board.append(tile.new("BUMHall", ["BillardsRoom","BallRoom"], ["Up","Down"],true,   Vector3( -2.4 ,0, -11.8)))
+	board.append(tile.new("BULHall", ["Library","Conservatory"], ["Up","Down"],true,    Vector3( 21.3 ,0, -11.8)))
+	board.append(tile.new("TDLHall", ["Study","Library"], ["Up","Down"],true,   Vector3( 21.5 ,0, 10.8)))
+	board.append(tile.new("TDMHall", ["Hall","BillardsRoom"], ["Up","Down"],true,   Vector3(-2.3,0,10.8)))
+	board.append(tile.new("TDRHall", ["Lounge","DinningRoom"], ["Up","Down"],true,   Vector3( -25 ,0, 10.8)))
 	
 	
 func get_room(room : String)->Tile:
