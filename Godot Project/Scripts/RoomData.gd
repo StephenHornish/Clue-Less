45e343f143d7601b99bfcce08660d638fcb15944
Stephen Hornish <hornishs18@gmail.com>
2023-02-06 08:05:33 -0800

    Put all the room in the database need to add halls

--- Godot Project/Scripts/RoomData.gd
+++ Godot Project/Scripts/RoomData.gd
@@ -11,6 +11,16 @@ func _ready():
 func _init():
 	board.append(tile.new("BallRoom", ["BLHAll","BUMHall","BRHall"], ["Left","Up","Right"],false,Vector3( -2.3 ,0, -22.5)))
 	board.append(tile.new("Conservatory", ["BLHAll","BULHall","Lounge"], ["Up","Right", "Secret Passage"],false,Vector3( 22 ,0, -22.5)))
+	board.append(tile.new("Library", ["MLHAll","BULHall","TDLHall"], ["Up","Right", "Down"],false,Vector3( 22 ,0, -0.5)))
+	board.append(tile.new("Study", ["TDLHall","TLHall","Kitchen"], ["Down","Right", "Secret Passage"],false,Vector3( 22 ,0, 22.3)))
+	board.append(tile.new("Hall", ["TLHall", "TDMHall","TRHall"], ["Left", "Down","Right"],false,Vector3( -2.3 ,0, 22.3)))
+	board.append(tile.new("Lounge", ["TLHall", "MRHall","Conservatory"], ["Left", "Down","Secret Passage"],false, Vector3( -26 ,0 , 21.6)))
+	board.append(tile.new("DinningRoom", ["TDRHall", "MRHall","BURHall"], ["Up", "Left","Down"],false, Vector3( -26 ,0, -0.5)))
+	board.append(tile.new("Kitchen", ["BURHall", "BRHall","Study"], ["Up", "Left","Secret Passage"],false, Vector3( -26 ,0, -22.5)))
+	board.append(tile.new("BillardsRoom", ["MLHall", "TDMHall","MRHall","BUMHall"], ["Left", "Up", "Right","Down"],false, Vector3( -2.3 ,0, -0.5)))
+	
+	
+	
 	
 func get_room(room : String)->Tile:
 	for tile in board:
